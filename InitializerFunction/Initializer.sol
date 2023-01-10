e1004e79fffed263d5e093f35c98740b6612f949
Anass Bali <118474278+AnassBali@users.noreply.github.com>
2023-01-10 21:55:55 +0100

    Create Initializer.sol

---
+++ InitializerFunction/Initializer.sol
@@ -0,0 +1,35 @@
+//BEFORE 
+
+   /// @notice initialize method for EIP712Upgradeable
+    /// @dev called once after initial deployment and every upgrade.
+    /// @param _name the user readable name of the signing domain for EIP712
+    /// @param _version the current major version of the signing domain for EIP712
+    function initializeEIP712(string memory _name, string memory _version)
+        public
+        initializer
+    {
+        name = _name;
+        version = _version;
+
+        __EIP712_init(_name, _version);
+    }
+
+
+//FIXED
+
+abstract contract EIP712MetaTransaction is EIP712Upgradeable {
+    // ...
+}
+    /// @notice initialize method for EIP712Upgradeable
+    /// @dev called once after initial deployment.
+    /// @param _name the user readable name of the signing domain for EIP712
+    /// @param _version the current major version of the signing domain for EIP712
+    function __EIP712MetaTransaction_init(string memory _name, string memory _version)
+        internal
+        onlyInitializing
+    {
+        name = _name;
+        version = _version;
+
+        __EIP712_init(_name, _version);
+    }
